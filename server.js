c44301d4b82aba25c5e86c6f2fc65d04a27ec3fe
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-05-08 23:40:04 -0700

    heroku log debugging

--- server.js
+++ server.js
@@ -20,15 +20,24 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget', {
-   /** useNewUrlParser false by default. Set to `true` to make all connections set the `useUnifiedTopology` option by default */
+  /** useNewUrlParser false by default. Set to `true` to make all connections set the `useUnifiedTopology` option by default */
   useNewUrlParser: true,
-    /** Set use FindAndModify to `true` to make Mongoose automatically call `createCollection()` on every model created on this connection. */
+  /** Set use FindAndModify to `true` to make Mongoose automatically call `createCollection()` on every model created on this connection. */
   useFindAndModify: false,
   useUnifiedTopology: true
 });
 
 // routes
 app.use(require('./routes'));
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ChristiLewis:Wlrdy4pkxWj7dabC@cluster0.ihk9u.mongodb.net/budget?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 //LOG MONGO QUERIES EXECUTED
 mongoose.set('debug', true);
